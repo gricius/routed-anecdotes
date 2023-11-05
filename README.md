dd80f782fdb38d95b033bfb11ca21f30833e617f
gricius <gricius@gmail.com>
2023-11-05 22:14:10 +0100

    add 7.2: routed anecdotes, step2

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ The Footer component should always be visible at the bottom.
 
 The creation of a new anecdote should happen e.g. in the path create:
 ![footer](https://fullstackopen.com/static/c393db40b64e8eadd1220bdfccc8eede/5a190/41.png)
+
+# 7.2: routed anecdotes, step2
+Implement a view for showing a single anecdote:
+
+![show sinlgle anecdote](https://fullstackopen.com/static/3287ad77ebb90dfac2d734d9801b20b0/5a190/42.png)
+
+Navigating to the page showing the single anecdote is done by clicking the name of that anecdote:
+
+![show single anecdote](https://fullstackopen.com/static/116f966d64a03287b86a6e6a03f6ba81/5a190/43.png)
--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,33 @@ import { useState } from 'react'
 import {
   Routes,
   Route,
-  Link} from 'react-router-dom'
+  Link,
+  useParams
+} from 'react-router-dom'
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+      <li key={anecdote.id}>
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find(a => a.id === Number(id))
+  return (
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -127,6 +144,7 @@ const App = () => {
         <Link style={padding} to='/about'>about</Link>
       </div>
       <Routes>
+        <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route path='/create' element={<CreateNew addNew={addNew} />} />
         <Route path='/about' element={<About />} />
